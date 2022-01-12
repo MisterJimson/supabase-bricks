9b10443c7d158dc0c5ea2cebd405eaeffd12917f
Jason Rai <me@jasonrai.ca>
2022-01-11 19:31:45 -0500

    fix: minor fixes

--- bricks/flutter_user_management/__brick__/flutter-user-management/lib/utils/constants.dart
+++ bricks/flutter_user_management/__brick__/flutter-user-management/lib/utils/constants.dart
@@ -1,4 +1,5 @@
-const myAuthRedirectUri = 'com.{{organization_name}}.{{flutter_package_name}}://login-callback';
+const myAuthRedirectUri =
+    'com.{{organization_name}}.{{flutter_package_name}}://login-callback';
 
-const supabaseUrl = '{{supabase_url}}';
+const supabaseUrl = '{{{supabase_url}}}';
 const supabaseAnnonKey = '{{supabase_anon_public_key}}';
