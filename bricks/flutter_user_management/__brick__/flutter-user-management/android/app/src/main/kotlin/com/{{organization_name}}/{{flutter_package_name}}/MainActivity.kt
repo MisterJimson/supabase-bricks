890093c68979a3a653d49378c0bf32a06dbd7fea
Jason Rai <me@jasonrai.ca>
2022-01-11 19:14:08 -0500

    feat: add flutter_user_management brick

---
+++ bricks/flutter_user_management/__brick__/flutter-user-management/android/app/src/main/kotlin/com/{{organization_name}}/{{flutter_package_name}}/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.{{organization_name}}.{{flutter_package_name}}
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ bricks/flutter_user_management/__brick__/flutter-user-management/lib/utils/constants.dart
@@ -0,0 +1,4 @@
+const myAuthRedirectUri = 'com.{{organization_name}}.{{flutter_package_name}}://login-callback';
+
+const supabaseUrl = '{{supabase_url}}';
+const supabaseAnnonKey = '{{supabase_anon_public_key}}';
